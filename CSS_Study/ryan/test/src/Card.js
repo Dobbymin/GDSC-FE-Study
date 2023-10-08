e239bf44588edda690878b71510720b4930ce315
Dobbymin <gmin0701@knu.ac.kr>
2023-10-08 22:07:14 +0900

    ✨feat: tailwindcss 공부

---
+++ CSS_Study/ryan/test/src/Card.js
@@ -0,0 +1,15 @@
+function Card() {
+    return (
+        <div>
+            <button
+                class="px-4 py-2 
+                bg-indigo-600 rounded 
+                text-white"
+            >
+                버튼1
+            </button>
+        </div>
+    );
+}
+
+export default Card;

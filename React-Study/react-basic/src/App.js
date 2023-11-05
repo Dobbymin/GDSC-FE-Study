5fe86870284f40f64fa5e6ac124b648edc2d7898
Dobbymin <gmin0701@knu.ac.kr>
2023-11-05 10:06:44 +0900

    ✨ feat: useState 구현

---
+++ React-Study/react-basic/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+    const [number, setNumber] = useState(1);
+
+    /** 들어온 숫자들을 2배로 만들어준다. */
+    const double = () => {
+        // const doubleNumber = number * 2;
+        setNumber((prevState) => prevState * 2);
+        setNumber((prevState) => prevState * 2);
+    };
+
+    return (
+        <>
+            <div>{number}</div>
+            <button onClick={double}>Submit</button>
+        </>
+    );
+}
+
+export default App;
